752af74b4c858d7a319c1c8e25cc7f20e603c8f3
aneliabg10@gmail.com <aneliabg10@gmail.com>
2023-05-29 00:52:08 +0200

    adding ruler pipeline

--- src/ruler.py
+++ src/ruler.py
@@ -3,3 +3,30 @@ import spacy
 import pandas as pd
 
 #%%
+df = pd.read_csv("data/stocks.tsv", sep="\t")
+
+symbols = df.Symbol.tolist()
+companies = df.CompanyName.tolist()
+print (symbols[:10])
+
+#%%
+stops = ["two"]
+nlp = spacy.blank("en")
+ruler = nlp.add_pipe("entity_ruler")
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+patterns = []
+for symbol in symbols:
+    patterns.append({"label": "STOCK", "pattern": symbol})
+    for l in letters:
+        patterns.append({"label": "STOCK", "pattern": symbol+f".{l}"})
+for company in companies:
+    if company not in stops:
+        patterns.append({"label": "COMPANY", "pattern": company})
+
+#%%
+
+doc = nlp(text)
+for ent in doc.ents:
+    print (ent.text, ent.label_)
+
+#%%
