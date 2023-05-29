26f8c6e13333f1d45a0b14ddabbcbc8b8c1dc2a7
AneliaAB <aneliabg10@gmail.com>
2023-05-29 17:35:10 +0200

    prompt added

--- src/ruler.py
+++ src/ruler.py
@@ -3,6 +3,7 @@ import spacy
 import pandas as pd
 import os
 import pickle
+from spacy import displacy
 
 #%%
 df = pd.read_csv(os.path.join("..", "data", "stocks.tsv"), sep="\t")
@@ -29,53 +30,61 @@ ruler.add_patterns(patterns)
 
 #%%
 def extract_stock():
-    #create corpus from data
+    #load vectorizer and model
+    loaded_vectorizer = pickle.load(open('../models/vectorizer.pickle', 'rb'))
+    loaded_model = pickle.load(open('../models/classification.model', 'rb'))
+
+    #create corpus
     data = pd.read_csv(os.path.join("..","in","stock_data.csv"))
     corpus = []
 
     for ind in data.index:
         text = data['Text'][ind]
         corpus.append(text)
-
-    #%%
-    #extract stock name from text
+    
+    #generate stock/company name
+    stock = input("Name: ")
+    lable = input("Type: ")
+    
+    
     for text in corpus:
         doc = nlp(text)
 
         for ent in doc.ents:
             entity = ent.text
             lable = ent.label_
-    
-    loaded_vectorizer = pickle.load(open('../models/vectorizer.pickle', 'rb'))
-
-    # load the model
-    loaded_model = pickle.load(open('../models/classification.model', 'rb'))
-
-    stock = input("Name of stock or company: ")
-    
-    for text in corpus:
-        if stock in text:
-            test_sentence = loaded_vectorizer.transform([text])
-            print(loaded_model.predict(test_sentence))
+        
+            if entity == stock in text:
+                if lable == lable.upper():
+                    displacy.render(doc, style="ent")
+                    test_sentence = loaded_vectorizer.transform([text])
+            
+                    print(loaded_model.predict(test_sentence))
 
 #%%
 extract_stock()
 
 # %%
-#generates a prediction based on prompt
-def classify_sentance():
-    entity, lable = extract_stock()
+def create_df(path_to_folder):
+    filenames = os.listdir(path_to_folder)
 
-    # load the vectorizer
-    loaded_vectorizer = pickle.load(open('../models/vectorizer.pickle', 'rb'))
+    categories = []
+    for filename in filenames:
+        if path_to_folder == "../data/bleached_corals":
+            categories.append(0)
+        else:
+            categories.append(1)
 
-    # load the model
-    loaded_model = pickle.load(open('../models/classification.model', 'rb'))
+    df = pd.DataFrame({
+        'filename': filenames,
+        'category': categories
+    })
+    
+    return df
 
-    tweet = input("Write a sentance to generate lable with Neural Network classifier: ")
-    test_sentence = loaded_vectorizer.transform([sentence])
-    print(loaded_model.predict(test_sentence))
+healthy_df = create_df("../data/healthy_corals")
+bleached_df = create_df("../data/bleached_corals")
 
-# %%
-#classify_sentance()
-#NKD looking like a good short. Failed to break price level resistance at 116 today yealds a negative sentiment
+df = pd.concat([bleached_df, healthy_df])
+df_plt = df["category"].replace({0: 'bleached', 1: 'healthy'}).value_counts().plot.bar() 
+df_plt.figure.savefig("../out/visualizing_dataframe.png", dpi=300, bbox_inches='tight') # specify filetype explicitly
